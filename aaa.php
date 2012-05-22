d88afc9742d15f568e0793e1d80b43ec98f74fb2
chrowa3 <hogehogehoge@webya.in>
2012-05-23 02:21:07 +0900

    add aaa.php

---
+++ aaa.php
@@ -0,0 +1,56 @@
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'."\n" ?>
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+
+<head>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    
+    <meta http-equiv="Content-Style-Type" content="text/css" />
+    
+    <meta http-equiv="Content-Script-Type" content="text/javascript" />
+    
+    <meta name="description" content="ＸＸＸ" />
+    
+    <meta name="keywords" content="ＸＸＸ" />
+    
+    <link rev="made" href="mailto:stratoblackie1978@yahoo.co.jp" />
+    
+    <link rel="start" href="./index.html" />
+        
+    <title>テンプレートデータ</title>
+    
+    <link href="css/common.css" rel="stylesheet" type="text/css" />
+    
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    
+    <link href="css/fonts.css" rel="stylesheet" type="text/css" />
+ 
+    <!--[if IE 6]>
+    
+        <link rel="stylesheet" href="css/style_ie6.css" type="text/css" />
+    
+    <![endif]--> 
+    
+    <!--[if IE 7]>
+    
+        <link rel="stylesheet" href="css/style_ie7.css" type="text/css" />
+    
+    <![endif]--> 
+    
+
+    
+    
+</head>
+
+
+<body>
+
+
+</body>
+
+
+</html>
